79b98c1e4fa7b2f7ab41cd65760e4655291b0838
Hou Teng Edward Lam <lamhoutenged@seattleu.edu>
2023-02-21 23:07:06 +0800

    Merge my project to William's branch

--- src/main/java/com/doConnect/controller/UserController.java
+++ src/main/java/com/doConnect/controller/UserController.java
@@ -1,5 +1,67 @@
+/**
+ * 
+ */
 package com.doConnect.controller;
 
-public class UserController {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.User;
+
+/**
+ * @author : Edward Lam
+ * @date   : 2023-02-20
+ */
+public class UserController {
+	@GetMapping("/")
+	public void Home(){
+		
+	}
+	
+	@GetMapping("/adduser")
+	public void addUser() {
+		
+	}
+	
+	@GetMapping("/addnewuser")
+	public void addNewUser(){
+		
+	}
+	
+	@GetMapping("/getLogin")
+	public void getLogin(){
+		
+	}
+	
+	@GetMapping("/getallusers")
+	public List<User> getAllUser(){
+		return new ArrayList<User>();
+	}
+	
+	@GetMapping("/getuserbyId")
+	public User getUserbyId(int uid) {
+		return new User();
+	}
+	
+	@GetMapping("/updateuser")
+	public void updateUser() {
+		
+	}
+	
+	@GetMapping("/getbyname")
+	public void getbyName() {
+		
+	}
+	
+	@GetMapping("/getbyalluserType")
+	public List<User> getbyAllUserType(){
+		return new ArrayList<User>();
+	}
+	
+	@GetMapping("/loginVerify")
+	public void userLoginVerify() {
+		
+	}
 }
